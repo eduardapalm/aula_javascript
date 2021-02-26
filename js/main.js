f89b31b8b48efcc7252fe64cfcc4a4977f43700e
Eduarda Almeida <eduardapalm27@gmail.com>
2021-02-25 21:08:25 -0300

    adicionando arquivo JS

---
+++ js/main.js
@@ -0,0 +1,24 @@
+
+
+
+var lista = ["maça"," pera"," laranja"];
+console.log(lista);
+
+alert(lista)
+
+//var nome = "Maria Eduarda";
+//var idade = 20;
+//var idade2 = 21;
+//var n1 = 2;
+//var n2 = 5;
+//var frase = "Japão é o melhor time do mundo";
+//alert("Bem vinda " +nome+ " ao seu primeiro projeto JS");
+//alert(nome + " tem " +idade+ " anos.");
+//alert(idade + idade2);
+//console.log(nome);
+//console.log(idade + idade2);
+//console.log(n1 * n2);
+//console.log(frase.replace("Japão","Flamengo"));
+//console.log(frase.toLowerCase());
+//console.log(frase.toUpperCase());
+//alert(frase.replace("Japão","Flamengo"));
